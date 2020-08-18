31fc6cfbd6b922134b69e637a4270075530dd4f3
Anindya Chakrabarty <anindya.stps@gmail.com>
2020-08-18 21:54:04 +0100

    With Flask

--- WebApp.py
+++ WebApp.py
@@ -1,39 +1,8 @@
 
 
-import pickle
-import numpy as np
+
 from flask import Flask, request, jsonify, render_template
-import pickle
 
-import pandas as pd
-
-models = pickle.load(open('model.pkl','rb'))
-
-def predictGUI(self,models,newDataList):
-        import pandas as pd
-        import numpy as np
-        from statistics import mode
-        newData_=pd.DataFrame(newDataList).T
-        newData_.columns=['CreditScore', 'Geography', 'Gender', 'Age', 'Tenure', 'Balance', 'NumOfProducts', 'HasCrCard', 'IsActiveMember', 'EstimatedSalary']
-        processedDataHeaders=['CreditScore','Age','Tenure',	'Balance','NumOfProducts', 'HasCrCard', 'IsActiveMember', 'EstimatedSalary','Geography_Germany','Geography_Missing','Geography_Spain','Gender_Male','Gender_Missing']
-        newData_['CreditScore']=float(newData_['CreditScore'])
-        newData_['Age']=float(newData_['Age'])
-        newData_['Tenure']=int(newData_['Tenure'])
-        newData_['Balance']=int(newData_['Balance'])
-        newData_['NumOfProducts']=int(newData_['NumOfProducts'])
-        newData_['HasCrCard']=int(newData_['HasCrCard'])
-        newData_['IsActiveMember']=int(newData_['IsActiveMember'])
-        newData_['EstimatedSalary']= float(newData_['EstimatedSalary'])
-        
-        newData_ = pd.get_dummies(newData_,drop_first=False)
-        newData_= newData_.reindex(columns=processedDataHeaders,fill_value=0)
-        finalPred=[]
-        for key in models:
-            print(key)
-            finalPred.append(int(models[key].predict(newData_)))
-            print(int(models[key].predict(newData_)))
-
-        return mode(finalPred)
 
 webapp = Flask(__name__)
 
