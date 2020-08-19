043003247cd15773c04f859a2b5b6105fa6d34c3
Anindya Chakrabarty <anindya.stps@gmail.com>
2020-08-19 21:25:37 +0100

    19 AUG

--- WebApp.py
+++ WebApp.py
@@ -6,9 +6,11 @@ from flask import Flask, request, jsonify, render_template
 
 webapp = Flask(__name__)
 
+
 @webapp.route('/')
+@webapp.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('/home.html')
 
 
 @webapp.route('/predict',methods=['POST'])
@@ -17,6 +19,7 @@ def predict():
     import pandas as pd
     import numpy as np
     from statistics import mode
+    from astropy.table import QTable, Table, Column
     models = pickle.load(open('model.pkl','rb'))
     CreditScore=float(request.form.get('CreditScore'))
     Geography=request.form.get('Geography')
@@ -37,11 +40,29 @@ def predict():
     finalPred=[]
     for key in models:
         finalPred.append(key)
-        finalPred.append(int(models[key].predict(newData_)))
+        if int(models[key].predict(newData_))==1:
+            verdict="Customer will Exit"
+        else:
+            verdict ="Customer will not Exit"
+        finalPred.append(verdict)
         
     finalPred.append("Final Verdict :")
-    finalPred.append(mode(finalPred)) 
-    return render_template('index.html', prediction_text=finalPred)
+    finalPred.append(mode(finalPred))
+    table=[]
+    table.append(f'{finalPred[0]} : {finalPred[1]}')
+    table.append(f'{finalPred[2]} :{finalPred[3]}')
+    table.append(f' {finalPred[4]} :{finalPred[5]}')
+    table.append(f'{finalPred[6]}:{finalPred[7]}')
+    table.append(f'{finalPred[8]} :{finalPred[9]}')
+    table.append(f'{finalPred[10]} :{finalPred[11]}')
+    table.append(f'{finalPred[12]} :{finalPred[13]}')
+    table.append(f'{finalPred[14]} : {finalPred[15]}')
+    table.append(f'{finalPred[16]} : {finalPred[17]}')
+      
+    
+    return render_template('home.html', prediction_text=table)
+
+
 
 if __name__ == "__main__":
     webapp.run(debug=True)
